9927518ce8ad292d4f6f9995b4e3d65710a5cfac
Garcez <allan.f.garcez@gmail.com>
2018-04-30 02:37:37 -0300

    REGEX PATTERNS + Slug|Camel|SneakCase

--- src/main/java/Capitalize.java
+++ src/main/java/Capitalize.java
@@ -1,7 +1,27 @@
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Capitalize {
 
+    private static final String ALL_UPPER_CASE = "A-Z";
+    private static final String ALL_SPECIAL_UPPER_CASE = ALL_UPPER_CASE + "ÇÁÀÃÂÂÉÈẼÊÌÍĨÎÕÓÒÔŨÚÙÜÛ";
+    private static final String ALL_SPECIAL_LOWER_CASE = "a-zcáàãâéèẽêìíĩîõóòôũúùüû";
+    private static final String REGEX_ALL_UPPER_CASE = "[" + ALL_SPECIAL_UPPER_CASE + "]";
+    private static final String REGEX_ALL_LOWER_CASE = "[" + ALL_SPECIAL_LOWER_CASE + "]";
+
+    private static Matcher getMatcher(String string) {
+        return Pattern
+                .compile(REGEX_ALL_UPPER_CASE + "{2,}(?=" + REGEX_ALL_UPPER_CASE + REGEX_ALL_LOWER_CASE +
+                        "+[0-9]*|\\b)|" + REGEX_ALL_UPPER_CASE + "?" + REGEX_ALL_LOWER_CASE
+                        + "+[0-9]*|" + REGEX_ALL_UPPER_CASE + "|[0-9]+")
+                .matcher(string);
+    }
+
+    /**
+     * @param sentence
+     * @return
+     */
     public static String capitalizeByWords(String sentence) {
         StringBuilder newString = new StringBuilder();
         try {
@@ -18,6 +38,7 @@ public final class Capitalize {
 
     public static String brazillianCapitalize(String palavra) {
         return Objects.requireNonNull(capitalizeByWords(palavra))
+                .replaceAll(" E ", " e ")
                 .replaceAll(" Da ", " da ")
                 .replaceAll(" Das ", " das ")
                 .replaceAll(" De ", " de ")
@@ -27,4 +48,46 @@ public final class Capitalize {
                 ;
     }
 
+    public static String toCamelCase(String string) {
+        StringBuilder newString = new StringBuilder();
+        Matcher m = getMatcher(string);
+        while (m.find()) {
+            newString.append(
+                    m.group(0).substring(0, 1)
+                            .toUpperCase()).append(m.group(0)
+                    .substring(1).toLowerCase()
+            );
+        }
+        return String.valueOf(newString).trim();
+    }
+
+    public static String toSneakCase(String string) {
+        StringBuilder newString = new StringBuilder();
+        Matcher m = getMatcher(string);
+        while (m.find())
+            newString.append(m.group(0).toLowerCase().concat("_"));
+        return newString.toString()
+                .trim()
+                .replaceAll("_$", "");
+    }
+
+    public static String toSlugCase(String string) {
+        StringBuilder newString = new StringBuilder();
+        Matcher m = getMatcher(string);
+        while (m.find())
+            newString.append(m.group(0).toLowerCase().concat("-"));
+        return newString.toString()
+                .trim()
+                .replaceAll("-$", "");
+    }
+
+    public static String camelCaseToAnother(String string, String replace) {
+        String replacement = "$1" + replace + "$2";
+        return string
+                .replaceAll("([" + ALL_SPECIAL_LOWER_CASE + "\\d])" +
+                        "(" + REGEX_ALL_UPPER_CASE + ")", replacement)
+                .replaceAll("(" + REGEX_ALL_UPPER_CASE + "+)" +
+                        "(" + REGEX_ALL_UPPER_CASE + "[" +
+                        ALL_SPECIAL_LOWER_CASE + "\\d]+)", replacement);
+    }
 }
--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -1,36 +1,59 @@
+import freemarker.template.utility.StringUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tests {
     public static void main(String[] args) {
         for (String name : dumpNames()) {
-            testAll(name, "New Capitalize Method -->");
-            testAllWithOld(name, "Old Capitalize Method");
+            testAll(name);
+            toCamelCase(name);
+            toSneakCase(name);
+            toSlugCase(name);
         }
     }
 
-    public static String testAll(String name, String message) {
+    public static void testAll(String name) {
         long init = System.currentTimeMillis() % 1000;
-        String capitalize = Capitalize.capitalizeByWords(name);
+        String capitalize = Capitalize.brazillianCapitalize(name);
         long end = System.currentTimeMillis() % 1000;
-        System.out.println(end - init + "ms. Total chars: " + name.length() + ". " + message + " : " + capitalize);
-        return capitalize;
+        System.out.println(end - init + "ms. " + capitalize);
     }
 
-    public static String testAllWithOld(String name, String message) {
+    public static void concorrente(String name) {
         long init = System.currentTimeMillis() % 1000;
-        String capitalize = OldCapitalize.capitalize(name);
+        String capitalize = StringUtil.capitalize(name);
         long end = System.currentTimeMillis() % 1000;
-        System.out.println(end - init + "ms. Total chars: " + name.length() + ". " + message + " : " + capitalize);
-        return capitalize;
+        System.out.println(end - init + "ms. " + capitalize);
+    }
+
+    public static void toSlugCase(String name) {
+        long init = System.currentTimeMillis() % 1000;
+        String capitalized = Capitalize.toSlugCase(name);
+        long end = System.currentTimeMillis() % 1000;
+        System.out.println(end - init + "ms. Total chars: " + capitalized);
+    }
+
+    public static void toCamelCase(String name) {
+        long init = System.currentTimeMillis() % 1000;
+        String capitalized = Capitalize.toCamelCase(name);
+        long end = System.currentTimeMillis() % 1000;
+        System.out.println(end - init + "ms. Total chars: " + capitalized);
+    }
+
+    public static void toSneakCase(String name) {
+        long init = System.currentTimeMillis() % 1000;
+        String capitalized = Capitalize.toSneakCase(name);
+        long end = System.currentTimeMillis() % 1000;
+        System.out.println(end - init + "ms. Total chars: " + capitalized);
     }
 
     public static List<String> dumpNames() {
         List<String> list = new ArrayList<>();
+        list.add("Luiz Inácio da Silva");
         list.add("tomás turbando nassuakara");
         list.add("pedro de alcântara joão carlos leopoldo salvador bibiano francisco xavier de paula leocádio miguel gabriel rafael gonzaga");
         list.add("pedro de alcântara joão carlos leopoldo salvador bibiano francisco xavier de paula leocádio miguel gabriel rafael gonzaga da silva lima soares reis de moraes tristão rodrigues fernandes freitas garcez caetano prestes muller e abreu");
-        list.add("Luiz Inácio da Silva");
         list.add("James Gosling");
         list.add("Dilma Vana Rousseff");
         list.add("Grace Murray Hopper");
